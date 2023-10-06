0529125eab6d97a439ce65287ce9b8e30af38052
Nazar Krylov <98321579+HoldinSky@users.noreply.github.com>
2023-10-06 08:50:13 +0300

    clean unnecessary comments

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -1,11 +1,5 @@
 use crate::menu::menu;
 
-/*
-    "/home/nazar/programming/rust/tik_lab1/samples/en/test_1.txt"
-    "/home/nazar/programming/rust/tik_lab1/samples/en/test_2.txt"
-    "/home/nazar/programming/rust/tik_lab1/samples/en/test_3.txt"
-*/
-
 pub fn start() {
     menu();
 }
