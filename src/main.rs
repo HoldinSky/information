9b0460419d98b0e9cbdb0786aff3a5f8d1d84ef8
nazar <nkrylov2004@gmail.com>
2023-10-08 14:49:40 +0300

    feature: change directories

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate strum;
 extern crate strum_macros;
 
+use std::env;
+use std::os::unix::prelude::OsStringExt;
+
 mod application;
 mod menu;
 mod statistic;
@@ -13,4 +16,22 @@ fn main() {
     application::start();
 }
 
+fn print_entries_of_current_dir() {
+    println!("Entries in {:?}:", env::current_dir().unwrap());
+    for file in terminal::list_files().unwrap() {
+        println!("{}", String::from_utf8(file.into_vec()).unwrap());
+    }
+}
 
+fn test_directory_change() {
+    print_entries_of_current_dir();
+
+    terminal::change_dir("..");
+    print_entries_of_current_dir();
+
+    terminal::change_dir("..");
+    print_entries_of_current_dir();
+
+    terminal::change_dir("/opfwe");
+    print_entries_of_current_dir();
+}
--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -1,4 +1,10 @@
+use std::{env, fs};
+use std::error::Error;
+use std::ffi::OsString;
+use std::fmt::{Display, Formatter};
+use std::fs::DirEntry;
 use std::io::{Read, stdin, stdout, Write};
+use std::path::{Path, PathBuf};
 
 pub fn take_input() -> Vec<u8> {
     print!("# ");
@@ -22,4 +28,56 @@ pub fn take_line() -> Vec<u8> {
     stdin.read_line(&mut buffer).expect("Could not read input");
 
     buffer.into_bytes()
+}
+
+fn get_current_dir() -> Result<PathBuf, Box<dyn Error>> {
+    let dir = env::current_dir()?;
+    Ok(dir)
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(true)
+}
+
+pub struct BadPathError {
+    path: String,
+}
+
+impl BadPathError {
+    pub fn new(path: &str) -> Self {
+        Self { path: String::from(path) }
+    }
+}
+
+impl Display for BadPathError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Could not find directory: {}", self.path)
+    }
+}
+
+pub fn change_dir(new_dir: &str) -> Result<(), BadPathError> {
+    match env::set_current_dir(Path::new(new_dir)
+        .canonicalize().unwrap_or(env::current_dir().unwrap())
+    ) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(BadPathError::new(new_dir)),
+    }
+}
+
+pub fn list_files() -> Result<Vec<OsString>, Box<dyn Error>> {
+    let current_dir = get_current_dir()?;
+
+    let dir = fs::read_dir(current_dir)?;
+
+    let visible_entries: Vec<_> = dir
+        .filter_map(|entry| entry.ok())
+        .filter(|e| !is_hidden(&e))
+        .map(|e| e.file_name())
+        .collect();
+
+    Ok(visible_entries)
 }
